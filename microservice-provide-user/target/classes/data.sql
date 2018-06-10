40187392a932724ce45986cab819bc5c30f2fe9b
can <canvnac@gmail.com>
2018-06-10 22:13:17 +0800

    删除target下文件

--- microservice-provide-user/target/classes/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO user (id, username, name, age, balance)
-VALUES (1, 'a1', '张三', 20, 100.00);
-INSERT INTO user (id, username, name, age, balance)
-VALUES (2, 'a2', '李四', 28, 102.00);
-INSERT INTO user (id, username, name, age, balance)
-VALUES (3, 'a3', '王五', 32, 106.00);
